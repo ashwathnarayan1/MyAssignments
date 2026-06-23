3ee9f4be88b00b1398fb723cf01e33c0e6f1bef0
ashwathnarayan1 <108739928+ashwathnarayan1@users.noreply.github.com>
2026-06-23 18:22:29 +0530

    Add files via upload
    Day 1 assignment

---
+++ Day1/scopingAndVariables.js
@@ -0,0 +1,29 @@
+genderType = "male"
+function printGender() {
+   let color = "brown"
+    if (genderType === 'female') {
+        var age = 30
+        let color = "pink"
+        console.log(color);
+
+    }
+    console.log(age);
+    
+}
+
+printGender()
+console.log(genderType);
+
+/********************************************************************************** */
+
+
+const browserName = 'chrome'
+function getBrowserName(){
+
+    if (browserName==='chrome') {
+        let browserName = 'firefox'
+    }
+    console.log(browserName);
+    
+}
+getBrowserName()
